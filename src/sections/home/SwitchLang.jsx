e265841c0f60c4c82e196088ebb2fe909d2c1772
II-EMMA <emma.ahmedera@gmail.com>
2025-07-31 00:03:00 +0300

    text change

--- src/sections/home/SwitchLang.jsx
+++ src/sections/home/SwitchLang.jsx
@@ -28,7 +28,7 @@ export default function SwitchLangDropdown() {
       <select
         onChange={handleChangeLocale}
         value={currentLocale}
-        className=" block appearance-none w-full border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded-full shadow leading-tight focus:outline-none focus:shadow-outline text-white bg-transparent "
+        className=" block appearance-none text-base w-full border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded-full shadow leading-tight focus:outline-none focus:shadow-outline text-white bg-transparent "
       >
         <option value="en" className="text-black text-sm">
           EN
