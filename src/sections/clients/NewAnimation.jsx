27c0017f937042cc11abdcc5cc254488a9cee4e5
II-EMMA <emma.ahmedera@gmail.com>
2025-08-06 02:06:13 +0300

    animation changes

--- src/sections/clients/NewAnimation.jsx
+++ src/sections/clients/NewAnimation.jsx
@@ -2,12 +2,9 @@
 import { motion } from "framer-motion";
 import { useLocale, useTranslations } from "next-intl";
 import Logo from "../../assets/images/new-company-logo.png";
+import { useState } from "react";
 
 const images = [
-  { src: Logo.src, alt: "Company Logo", style: "max-w-[180px] max-h-[180px]" },
-  { src: Logo.src, alt: "Company Logo", style: "max-w-[180px] max-h-[180px]" },
-  { src: Logo.src, alt: "Company Logo", style: "max-w-[180px] max-h-[180px]" },
-  { src: Logo.src, alt: "Company Logo", style: "max-w-[180px] max-h-[180px]" },
   { src: Logo.src, alt: "Company Logo", style: "max-w-[180px] max-h-[180px]" },
   { src: Logo.src, alt: "Company Logo", style: "max-w-[180px] max-h-[180px]" },
   { src: Logo.src, alt: "Company Logo", style: "max-w-[180px] max-h-[180px]" },
@@ -21,14 +18,14 @@ export default function NewAnimation() {
   const isRTL = lang === "ar";
   const t = useTranslations();
   const { subTitle } = t.raw("Clients");
-  const duplicatedImages = [...images, ...images, ...images, ...images];
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className="opacity-80 py-8">
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:gap-x-8 md:gap-y-11 gap-y-8 xl:gap-y-0">
+        <div className="flex flex-col lg:flex-row items-center justify-between md:gap-x-8 md:gap-y-11 gap-y-8 xl:gap-y-0">
           <h2
-            className={`text-purple-gradient-5 text-4xl font-extrabold text-center mx-2 sm:mx-0 ${
+            className={`text-purple-gradient-5 text-4xl font-extrabold text-center mx-2 sm:mx-0 text-nowrap ${
               isRTL ? "2xl:mr-10 xl:mr-5 ml-0" : "2xl:ml-10 xl:ml-5 mr-0"
             }`}
           >
@@ -36,35 +33,40 @@ export default function NewAnimation() {
           </h2>
 
           <div
-            className={`overflow-hidden w-full bg-[#38c295]/2 ${
-              isRTL
-                ? "[mask-image:linear-gradient(to_left,_transparent,_black_25%,_black_75%,_transparent)]"
-                : "[mask-image:linear-gradient(to_right,_transparent,_black_25%,_black_75%,_transparent)]"
-            }`}
+            className={`overflow-x-hidden w-full bg-[#38c295]/2 `}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
           >
-            <motion.div
+            <div
               className={`flex gap-14 items-center ${
                 isRTL ? "pl-14" : "pr-14"
               }`}
-              animate={{
-                translateX: isRTL ? "50%" : "-50%",
-              }}
-              transition={{
-                duration: 20,
-                repeat: Infinity,
-                ease: "linear",
-                repeatType: "loop",
-              }}
             >
-              {duplicatedImages.map((image, index) => (
-                <img
+              {images.map((image, index) => (
+                <motion.img
                   key={index}
                   src={image.src}
                   alt={image.alt}
                   className={`flex-shrink-0 ${image.style}`}
+                  animate={
+                    isHovered
+                      ? { translateX: isRTL ? "-30%" : "30%" }
+                      : { translateX: isRTL ? "100%" : "-100%" }
+                  }
+                  transition={
+                    isHovered
+                      ? {
+                          repeatDelay: 0,
+                          duration: 0.01,
+                        }
+                      : {
+                          repeat: Infinity,
+                          repeatDelay: 0.75,
+                        }
+                  }
                 />
               ))}
-            </motion.div>
+            </div>
           </div>
         </div>
       </div>
